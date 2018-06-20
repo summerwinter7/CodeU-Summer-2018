83eb196fbaf7159e5aff76df12c665f7ccb8dace
Robert M Freeman <robertfreeman@Roberts-MacBook-Air.local>
2018-06-20 16:53:27 -0400

    fixed changes

--- src/main/java/codeu/controller/ProfileServlet.java
+++ src/main/java/codeu/controller/ProfileServlet.java
@@ -8,49 +8,35 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
-
 public class ProfileServlet extends HttpServlet {
-    private UserStore userStore;
-    private User user;
+  private UserStore userStore;
+  private User user;
 
+  public void init() throws ServletException {
+    super.init();
+    setUserStore(UserStore.getInstance());
+  }
 
-    public void init() throws ServletException {
-      super.init();
-      setUserStore(UserStore.getInstance());
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    String username = (String) request.getSession().getAttribute("user");
+    user = UserStore.getInstance().getUser(username);
+
+    if (request.getSession().getAttribute("user") != null) {
+      String aboutMe = request.getParameter("aboutMe");
+      user.setAboutMe(aboutMe);
+      UserStore.getInstance().updateUser(user);
+      response.sendRedirect("/profile/" + username);
     }
-
-    void setUserStore(UserStore userStore) {
-      this.userStore = userStore;
-    }
-
-
-
-      @Override
-      public void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws IOException, ServletException {
-            request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
-      }
-
-      @Override
-      public void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws IOException, ServletException {
-                String username = (String) request.getSession().getAttribute("user");
-                if(username ==null) {
-                    response.sendRedirect("/login");
-                    return;
-                }
-                user = UserStore.getInstance().getUser(username);
-                if (user == null){
-                    response.sendRedirect("/login");
-                    return;
-                }
-
-
-                String aboutMe = request.getParameter("aboutMe");
-                user.setAboutMe(aboutMe);
-                UserStore.getInstance().updateUser(user);
-                response.sendRedirect("/profile/" + username);
-
-
-      }
+  }
 }
--- src/test/java/codeu/controller/ProfileServletTest.java
+++ src/test/java/codeu/controller/ProfileServletTest.java
@@ -5,7 +5,6 @@ import codeu.model.store.basic.UserStore;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.UUID;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,38 +16,58 @@ import org.mockito.Mockito;
 
 public class ProfileServletTest {
 
-      private ProfileServlet profileServlet;
-      private HttpServletRequest mockRequest;
-      private HttpSession mockSession;
-      private HttpServletResponse mockResponse;
-      private RequestDispatcher mockRequestDispatcher;
-      private UserStore mockUserStore;
-      @Before
-      public void setup() {
-        profileServlet = new ProfileServlet();
+  private ProfileServlet profileServlet;
+  private HttpServletRequest mockRequest;
+  private HttpSession mockSession;
+  private HttpServletResponse mockResponse;
+  private RequestDispatcher mockRequestDispatcher;
+  private UserStore mockUserStore;
 
-        mockRequest = Mockito.mock(HttpServletRequest.class);
-        //mockSession = Mockito.mock(HttpSession.class);
-        //Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+  @Before
+  public void setup() {
+    profileServlet = new ProfileServlet();
 
-        mockResponse = Mockito.mock(HttpServletResponse.class);
-        mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-        Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profile.jsp"))
-            .thenReturn(mockRequestDispatcher);
+    mockRequest = Mockito.mock(HttpServletRequest.class);
+    mockSession = Mockito.mock(HttpSession.class);
+    Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
 
-        mockUserStore = Mockito.mock(UserStore.class);
-        profileServlet.setUserStore(mockUserStore);
-      }
+    mockResponse = Mockito.mock(HttpServletResponse.class);
+    mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+    Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profile.jsp")).thenReturn(mockRequestDispatcher);
+    mockUserStore = Mockito.mock(UserStore.class);
+    profileServlet.setUserStore(mockUserStore);
+  }
 
-      @Test
-      public void testDoGet() throws IOException, ServletException {
-          profileServlet.doGet(mockRequest, mockResponse);
-          Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
-      }
-      
+  @Test
+  public void testDoGet() throws IOException, ServletException {
+    profileServlet.doGet(mockRequest, mockResponse);
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
 
+  public void testDoPost_ExistingUser() throws IOException, ServletException {
 
+    User user =
+        new User(
+            UUID.randomUUID(),
+            "test username",
+            "$2a$10$.e.4EEfngEXmxAO085XnYOmDntkqod0C384jOR9oagwxMnPNHaGLa",
+            Instant.now(), "test_aboutMe");
 
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");
+    Mockito.when(mockRequest.getParameter("password")).thenReturn("test password");
 
+    UserStore mockUserStore = Mockito.mock(UserStore.class);
+    Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(true);
+    Mockito.when(mockUserStore.getUser("test username")).thenReturn(user);
+    profileServlet.setUserStore(mockUserStore);
 
+    HttpSession mockSession = Mockito.mock(HttpSession.class);
+    Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+
+    profileServlet.doPost(mockRequest, mockResponse);
+
+    Mockito.verify(mockUserStore, Mockito.never()).addUser(Mockito.any(User.class));
+    Mockito.verify(mockSession).setAttribute("user", "test username");
+    Mockito.verify(mockResponse).sendRedirect("/conversations");
+  }
 }
