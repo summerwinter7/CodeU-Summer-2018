a35f0fc1692088202c473093ea5363038939d4bf
summerwinter7 <summer_gerry@brown.edu>
2018-07-12 11:34:49 -0400

    Added tests for null input

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -54,19 +54,19 @@ public class RegisterServlet extends HttpServlet {
       request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
       return;
     }
+    
+    String password = request.getParameter("password");
+    
+    if (username.length()==0 || password.length()==0) {
+    	request.setAttribute("error", "You must enter a username and password");
+        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+        return;
+    }
 
     if (userStore.isUserRegistered(username)) {
       request.setAttribute("error", "That username is already taken.");
       request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
       return;
-    }
-    
-    String password = request.getParameter("password");
-    
-    if (username.length()==0 || password.length()==0) {
-    	request.setAttribute("error", "The username and password cannot be empty");
-        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
-        return;
     }
 
     String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
